c5603071c77465c007b9bf64bfd1feb182811249
DELL <DELL@10.0.0.179>
2023-04-09 19:33:36 -0400

    commit push

--- src/Day2HW4/homework4.java
+++ src/Day2HW4/homework4.java
@@ -2,13 +2,10 @@ package Day2HW4;
 
 public class homework4 {
 
-	public static void main (String args[]) {
-		
-		System.out.println("homework 4");
-		
-		
-		
-	}
 	
+	public void push() {
+		
+		System.out.println("PUSH");
+	}
 	
 }
